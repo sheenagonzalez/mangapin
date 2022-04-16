1a43db7bd45d16a4b91736f3c651374423e5fd0b
Sheena Gonzalez <shngonzalez@gmail.com>
2022-04-16 14:31:45 -0700

    deploy changes

--- dist/_nuxt/static/1650144604/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmangapin\u002F_nuxt\u002Fstatic\u002F1650144604",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fmangapin\u002F",assetsPath:"\u002Fmangapin\u002F_nuxt\u002F",cdnURL:a}}}}(null));
